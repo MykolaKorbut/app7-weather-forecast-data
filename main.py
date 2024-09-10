25ba5829bfde26a447926c1e36ec8aff193134bf
mikolakorbut <nikolay.korbut@gmail.com>
2024-09-10 18:08:15 +0300

    Day 34: Add Sky condition, fix bags

--- main.py
+++ main.py
@@ -10,10 +10,25 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+# Check if place exist
+if place:
+    # Get the temperature/sky data
+    try:
+        filtered_data = get_data(place, days)
 
-d, t = get_data(place, days, kind)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperature,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-
-figure = px.line(x=d, y=t,
-                 labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.write("Please enter a correct place")
